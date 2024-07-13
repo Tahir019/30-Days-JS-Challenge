9b96d95d9f5faf2b0268f73e63d2cac71a23c962
TAHIR AHMAD <121556994+Tahir019@users.noreply.github.com>
2024-07-13 23:09:25 +0530

    Add files via upload

---
+++ Day1/index.js
@@ -0,0 +1,49 @@
+// DAY 1: Variable and Data Types
+
+//Activity 1: Variable Declaration
+
+//>Task 1:
+var num = 5;
+console.log(num);
+
+//>Task 2:
+let str = "ChaiAurCode";
+console.log(str);
+
+//Activity 2: Constant Declaration
+
+//>Task 3: 
+const student = true;
+console.log(student);
+
+//Activity 3: Data Types
+
+//>Task 4:
+var num1 = 5;
+var str1 = "Chai&Code";
+var bool1 = true;
+var obj = {
+  name: "Chai&Code",
+  age: 2,
+  author: "HiteshChaudhry",
+}
+var arr = [1, 2, 3, 4, 5];
+
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof bool1);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//Activity 4: Reassigning Variables
+
+//>Task 5:
+let val = 2;
+val = 4;
+console.log(val); // 4
+
+//Activity 5: Understanding const
+
+//>Task 6:
+const assign = "Chai";
+// assign = "Code"; //reassigning error
